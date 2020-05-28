9d7dfcf7e625b4048396e7c3ce80ca86a439cea1
akira.tanaka <akkyan4@gmail.com>
2020-05-28 09:03:13 +0900

    Initial Commit

---
+++ timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timer
+//
+//  Created by 田中昭 on 2020/05/28.
+//  Copyright © 2020 tanaka akira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
